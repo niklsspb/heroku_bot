bcd1d790b496a1bef4b3ab04cd19d0427672d4ac
Niklsspb <prof051@yandex.ru>
2019-12-11 01:25:05 +0300

    1

--- jobs.py
+++ jobs.py
@@ -50,9 +50,10 @@ def get_every_day():
         date_post = date_time.date()
         json_string = get_indexes(image)
         res = json.loads(json_string)
-        result = res['temp']['y_']
-        url_image = url + result[0] + '.jpg'
-        url_image="http://www.kartinki.me/pic/201506/1920x1200/kartinki.me-21699.jpg"
+        result = res['temp']['y']
+        #url_image = url + result[0] + '.jpg'
+        url_image=result[0]
+        #url_image="http://www.kartinki.me/pic/201506/1920x1200/kartinki.me-21699.jpg"
         return url_image
     except IndexError:
         return None
